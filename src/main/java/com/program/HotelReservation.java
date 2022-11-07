5f7f09c5e9526362f74ba46f53461faca18757b7
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-07 15:07:00 +0530

    Added: Ability to Find The cheapest Hotel Based On Date

--- src/main/java/com/program/HotelReservation.java
+++ src/main/java/com/program/HotelReservation.java
@@ -1,22 +1,62 @@
 package com.program;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.*;
 
 public class HotelReservation {
+	String hotelNam1="Lakewood",hotelNam2="Bridgewood",hotelNam3="Ridgewood";
+	 int regilrHotelPric1=110, regilrHotelPric2=160 ,regilrHotelPric3=220;
+	 int weekendHotelPric1=90, weekendHotelPric2=60,weekendHotelPric3=150;
+	 String weekend="SUNDAY";
+	 String weekend2="SATURDAY";
+	 String days;
+	public boolean HotelNames(int[] date1,int[] date2) {
+		try {
+			LocalDate someDate = LocalDate.of(date1[2], date1[1], date1[0]);
+			 days=someDate.getDayOfWeek().toString();
+			System.out.println(days);	
+			CheapHotel();
+		}
+		catch(Exception e) {
+		    System.out.println(e);
+		}
+		return true;
+		}
+	public void CheapHotel()  {
+		if (days==weekend || days==weekend2) {
+			if (weekendHotelPric1 > weekendHotelPric2 ) {
+				if (weekendHotelPric2 >weekendHotelPric3) {
+					System.out.println("The Cheapest Hotel is  "+hotelNam3+" "+weekendHotelPric3);	
+				}  else {
+					System.out.print("The Cheapest Hotel is  "+hotelNam2+" "+weekendHotelPric2);
+				}
+			} else  {
+				if (weekendHotelPric1 > weekendHotelPric3) {
+					System.out.println("The Cheapest Hotel is  "+hotelNam3+" "+weekendHotelPric3);
+				}  else {
+					System.out.println("The Cheapest Hotel is  "+hotelNam1+" "+weekendHotelPric1);
+				}
+			} 
+		} else { 
+			if (regilrHotelPric1 > regilrHotelPric2 ) {
+				if (regilrHotelPric2 > regilrHotelPric3) {
+					System.out.println("The Cheapest Hotel is  "+hotelNam3+" "+regilrHotelPric3);
+				}  else {
+					System.out.print("The Cheapest Hotel is  "+hotelNam2+" "+regilrHotelPric2);
+				}
+			} else  {
+				if (regilrHotelPric1 > regilrHotelPric3) {
+					System.out.println("The Cheapest Hotel is  "+hotelNam3+" "+regilrHotelPric3);
+				}  else {
+					System.out.println("The Cheapest Hotel is  "+hotelNam1+" "+regilrHotelPric1);
+				}
+			}
+		}
 
-	 HashMap<String,ArrayList<String>> hotelDetail=new HashMap<String,ArrayList<String>>();
-	public void hotelNames(String hotelName,String regularPrice,String weekEndPrice) {
-	ArrayList<String> price=new ArrayList<String>();
-	price.add("Regular Day Price -> "+regularPrice);
-	price.add("WeekEnd Day Price -> "+weekEndPrice);	
-	hotelDetail.put(hotelName, price);
-	}
+		}
 	public static void main(String args[])  {
 		HotelReservation obj =new HotelReservation();
-		obj.hotelNames("lakewood","110","150");
-		obj.hotelNames("Bridgewood", "60", "160");
-		System.out.println(obj.hotelDetail);
+				int date1[]= {12,10,2022};
+				int date2[]= {15,10,2022};
+		obj.HotelNames(date1,date2);
 	}
 }
