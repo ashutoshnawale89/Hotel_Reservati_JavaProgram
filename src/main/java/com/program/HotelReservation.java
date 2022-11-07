9af66954a8621851bfaba5a725b134803ff8e764
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-07 15:27:55 +0530

    Added: Find The cheapest Hotel By Good Rating

--- src/main/java/com/program/HotelReservation.java
+++ src/main/java/com/program/HotelReservation.java
@@ -1,21 +1,72 @@
 package com.program;
 
+import java.text.ParseException;
+import java.time.LocalDate;
 import java.util.*;
 
+
 public class HotelReservation { 
-	HashMap<ArrayList<String>,String> hotelDetail=new HashMap<ArrayList<String>,String>();
-	public void hotelNames(String hotelName,String regularPrice,String weekEndPrice,String star) {
-		ArrayList<String> price=new ArrayList<String>();
-		price.add("Hotel Name :"+hotelName);
-		price.add("Regular Day Price -> "+regularPrice);
-		price.add("WeekEnd Day Price -> "+weekEndPrice);	
-		hotelDetail.put( price,"The Rating is :"+star);
+	private String hotelNam1="Lakewood",hotelNam2="Bridgewood",hotelNam3="Ridgewood";
+	private int regilrHotelPric1=110, regilrHotelPric2=160 ,regilrHotelPric3=220;
+	private int weekendHotelPric1=90, weekendHotelPric2=60,weekendHotelPric3=150;
+	private String weekend="SUNDAY";
+	private String weekend2="SATURDAY";
+	private String days;
+	
+	
+	public void HotelNames(int[] date1,int[] date2) {
+		LocalDate someDate = LocalDate.of(date1[2], date1[1], date1[0]);
+		String days=someDate.getDayOfWeek().toString();
+		System.out.println(days);
+		if (weekend==days || weekend2==days ) {
+			System.out.println(hotelNam1+" WeekEndDays Hotel Rate is  "+90+" $");
+			System.out.println(hotelNam2+" WeekEndDays Hotel Rate is  "+50+" $");
+			System.out.println(hotelNam3+" WeekEndDays Hotel Rate is  "+150+" $");   }
+		else  {
+			System.out.println(hotelNam1+" WeekDays Hotel Rate is  "+regilrHotelPric1+" $");
+			System.out.println(hotelNam2+" WeekDays Hotel Rate is  "+regilrHotelPric2+" $");
+			System.out.println(hotelNam3+" WeekDays Hotel Rate is  "+regilrHotelPric3+" $"); 
+		}
 	}
-	public static void main(String args[])  {
+	public void Rating() {
+		int lakewood =3;
+		int bridgewood=4;
+		int ridgewood=5;
+		System.out.println(hotelNam1+"  Rating is "+lakewood+"  Star");
+		System.out.println(hotelNam2+"  Rating is "+bridgewood+"  Star");
+		System.out.println(hotelNam3+"  Rating is "+ridgewood+"  Star");
+	}
+	public void CheapestBestRating() {
+		int noOfHotel=3;
+		int lakewood =3, bridgewood=4 , ridgewood=5;
+		int sum=(lakewood+bridgewood+ridgewood)/noOfHotel;
+		int rate1, rate2, rate3;
+		if (days==weekend || days==weekend2) {
+			 rate1=weekendHotelPric1;
+			 rate2=weekendHotelPric2;
+			 rate3=weekendHotelPric3;
+		}    else  {
+			 rate1=regilrHotelPric1;
+			 rate2=regilrHotelPric2;
+			 rate3=regilrHotelPric3;	
+		}
+		if (lakewood >=sum) {
+			System.out.println("The Best Rated Hotel is  "+hotelNam1+" & Rate is "+rate1);
+		}
+		else if (bridgewood >=sum) {
+			System.out.println("The Best Rated Hotel is  "+hotelNam2+" & Rate is "+rate2);	
+			}
+		else if (ridgewood >=sum)  {
+			System.out.println("The Best Rated Hotel is  "+hotelNam3+" & Rate is "+rate3);
+		}
+		}   
+
+	public static void main(String args[]) throws ParseException {
 		HotelReservation obj =new HotelReservation();
-		obj.hotelNames("lakewood","110","150","3");
-		obj.hotelNames("Bridgewood", "60", "160","4");
-		obj.hotelNames("RidgeWood", "140", "180","5");
-		System.out.println(obj.hotelDetail);
+		// format put the date is (dd/mm/yyyy)
+				int date1[]= {11,11,2020};
+				int date2[]= {10,11,2020};
+		obj.HotelNames(date1,date2);
+		obj.CheapestBestRating();
 	}
 }
