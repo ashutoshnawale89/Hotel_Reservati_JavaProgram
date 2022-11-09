8613a77023ce3734a37ccfbe48690fb7fa2bd229
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-09 08:01:33 +0530

    Refactor;Ability To Add The Rate of Hotel For Reward Customer

--- src/main/java/com/program/HotelReservation.java
+++ src/main/java/com/program/HotelReservation.java
@@ -6,62 +6,37 @@ import java.util.*;
 
 
 public class HotelReservation { 
-	private String hotelNam1="Lakewood",hotelNam2="Bridgewood",hotelNam3="Ridgewood";
-	private int regilrHotelPric1=110, regilrHotelPric2=160 ,regilrHotelPric3=220;
-	private int weekendHotelPric1=90, weekendHotelPric2=60,weekendHotelPric3=150;
-	private String weekend="SUNDAY";
-	private String weekend2="SATURDAY";
-	private String days;
+	HashMap<String,ArrayList<String>> rewardCustomer=new HashMap<String,ArrayList<String>>();
 	
 	
-	public void hotelNames(int[] date1,int[] date2) {
-		LocalDate someDate = LocalDate.of(date1[2], date1[1], date1[0]);
-		String days=someDate.getDayOfWeek().toString();
-		System.out.println(days);
+	public void specialCustomerRate(String hotelName,String regularRate,String weekendRate,String rewardRateRegular,String rewardRateWeekend) {
+		ArrayList<String> rate=new ArrayList<String>();
+		rate.add("Weekend Rate is : "+weekendRate);
+		rate.add("Regular Rate is : "+regularRate);
+		rate.add("Reward Rate is :"+rewardRateRegular);
+		rate.add("Weekend Rate For Reward : "+ rewardRateWeekend);
+		rewardCustomer.put(hotelName, rate);
 	}
-	public void rating() {
-		int lakewood =3;
-		int bridgewood=4;
-		int ridgewood=5;
-		System.out.println(hotelNam1+"  Rating is "+lakewood+"  Star");
-		System.out.println(hotelNam2+"  Rating is "+bridgewood+"  Star");
-		System.out.println(hotelNam3+"  Rating is "+ridgewood+"  Star");
-	}
- 
-	public void bestRatingHotel() {
-		rating();
-		int noOfHotel=3;
-		int lakewood =3, bridgewood=4 , ridgewood=5;
-		int array[]= {lakewood,bridgewood,ridgewood };
-		int rate1, rate2, rate3;
-		if (days==weekend || days==weekend2) {
-			 rate1=weekendHotelPric1;
-			 rate2=weekendHotelPric2;
-			 rate3=weekendHotelPric3;
-		}    else  {
-			 rate1=regilrHotelPric1;
-			 rate2=regilrHotelPric2;
-			 rate3=regilrHotelPric3;	
-		}	
-		int index=array.length/2;
-		Arrays.sort(array);
-		if (array[index]==lakewood) {
-			System.out.println("The Best Rated Hotel is  "+hotelNam1+" & Price is "+rate1);
-		}
-		if (array[index]==bridgewood) {
-			System.out.println("The Best Rated Hotel is  "+hotelNam2+" & Price is "+rate2);
-		}
-		if (array[index]==ridgewood) {
-			System.out.println("The Best Rated Hotel is  "+hotelNam3+" & Price is "+rate3);
-	}
-		}
-						
+	
 	public static void main(String args[]) throws ParseException {
 		HotelReservation obj =new HotelReservation();
-		// format put the date is (dd/mm/yyyy)
+		 String hotelNam1="Lakewood",hotelNam2="Bridgewood",hotelNam3="Ridgewood";
+		 String regulrHotelPric1="110", regulrHotelPric2="160" ,regulrHotelPric3="180";
+		 String weekendHotelPric1="150", weekendHotelPric2="180",weekendHotelPric3="210";
+		 String rewardRegulrHotelPric1="80", rewardRegulrHotelPric2="110",rewardRegulrHotelPric3="40";
+		 String rewardWeekendHotelPric1="80", rewardWeekendHotelPric2="150",rewardWeekendHotelPric3="100";
+
+		 obj.specialCustomerRate(hotelNam1, regulrHotelPric1,weekendHotelPric1, rewardRegulrHotelPric1,rewardWeekendHotelPric1);
+		 obj.specialCustomerRate(hotelNam2, regulrHotelPric2,weekendHotelPric2, rewardRegulrHotelPric2,rewardWeekendHotelPric2);
+		 obj.specialCustomerRate(hotelNam3, regulrHotelPric3,weekendHotelPric3, rewardRegulrHotelPric3,rewardWeekendHotelPric3);
+		 
+		 for(Map.Entry m:obj.rewardCustomer.entrySet()) {
+			 System.out.println(m.getKey()+"  "+m.getValue());
+		 }
+		 // format put the date is (dd/mm/yyyy)
 				int date1[]= {10,11,2020};
 				int date2[]= {11,11,2020};
-		obj.hotelNames(date1,date2);
-		obj.bestRatingHotel();
+				
+		
 	}
 }
